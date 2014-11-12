76919ecbbba4d262a0132c82f8dadf6487b7cb7b
Chad Stout <chad.stout@gmail.com>
2014-11-11 19:50:46 -0600

    all I need is a little background music service example

---
+++ Background Music Service/Android/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+using BackgroundStreamingAudio.Services;
+
+
+namespace Mambo.Android
+{
+	[Activity (Label = "Mambo", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : AndroidActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Xamarin.Forms.Forms.Init (this, bundle);
+
+			var mambo = new Mambo ();
+			mambo.RestartButton.Clicked += HandleClicked;
+
+			SetPage (mambo.MamboPage);
+
+
+			SendAudioCommand(MamboService.ActionPlay);
+		}
+
+		private void SendAudioCommand(string action)
+		{
+			var intent = new Intent(action);
+			StartService(intent);
+		}
+
+		void HandleClicked (object sender, EventArgs e)
+		{
+			SendAudioCommand(MamboService.ActionStop);
+			SendAudioCommand(MamboService.ActionPlay);
+		}
+	}
+}
+
---
+++ Background Music Service/Android/Recievers/MusicBroadcastReceiver.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Media;
+using BackgroundStreamingAudio.Services;
+
+namespace BackgroundStreamingAudio.Receivers
+{
+	/// <summary>
+	/// This is a simple intent receiver that is used to stop playback
+	/// when audio become noisy, such as the user unplugged headphones
+	/// </summary>
+	[BroadcastReceiver]
+	[IntentFilter(new []{AudioManager.ActionAudioBecomingNoisy})]
+	public class MamboReceiver: BroadcastReceiver
+	{
+		public override void OnReceive(Context context, Intent intent)
+		{
+			if (intent.Action != AudioManager.ActionAudioBecomingNoisy)
+				return;
+
+			//signal the service to stop!
+			var stopIntent = new Intent(MamboService.ActionStop);
+			context.StartService(stopIntent);
+		}
+	}
+}
